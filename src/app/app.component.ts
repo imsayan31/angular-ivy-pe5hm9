a7d089864a1657cae54afe4d42b55aac433d027e
imsayan31 <sayantadeytraining@gmail.com>
2021-09-07 19:31:06 +0530

    map, tap, take tested

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, VERSION } from '@angular/core';
 import { from, of } from 'rxjs';
+import { map, take, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -12,10 +13,22 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     of(2, 4, 6, 8).subscribe(console.log);
 
-    from([10, 15, 6]).subscribe(
-      item => console.log(`resulting item ... ${item}`),
-      err => console.error(`err occured ${err}`),
-      () => console.log(`complete`)
-    )
+    from([10, 15, 6])
+      .pipe(
+        map(item => item * 2),
+        tap(item => console.log('emitted item ' + item)),
+        map(item => {
+          if (item === 12) {
+            throw new Error(`${item} detected.`);
+          }
+          return item;
+        }),
+        take(2)
+      )
+      .subscribe(
+        item => console.log(`resulting item ... ${item}`),
+        err => console.error(`err occured ${err}`),
+        () => console.log(`complete`)
+      );
   }
 }
